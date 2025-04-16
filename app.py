17a70f3f9d7703d913c056fa1c728104ffe36abd
Z O R O <90545080+guider23@users.noreply.github.com>
2025-04-16 15:27:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pickle
+import os
+
+
 
 app = Flask(__name__)
 
@@ -31,5 +34,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
