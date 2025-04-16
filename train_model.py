ec6c158e3c42e18729999bad939612762094c886
Z O R O <90545080+guider23@users.noreply.github.com>
2025-04-16 16:19:52 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,16 +3,13 @@ import sklearn.datasets
 from xgboost import XGBRegressor
 import pickle
 
-# Load California Housing dataset
 dataset = sklearn.datasets.fetch_california_housing()
 X = pd.DataFrame(dataset.data, columns=dataset.feature_names)
 Y = dataset.target
 
-# Train model
 model = XGBRegressor()
 model.fit(X, Y)
 
-# Save model
 with open("house_price_model.pkl", "wb") as file:
     pickle.dump(model, file)
 
